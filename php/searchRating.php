770f77b64def54feebd4624ed19d3ebd37a89fe9
Kurt Amodia <99860368+ainzzcutie@users.noreply.github.com>
2023-06-07 19:17:26 +0800

    new version with rating filter

--- php/searchRating.php
+++ php/searchRating.php
@@ -6,17 +6,10 @@
     // Modify the SQL query to include the search and rating filter conditions
     $sql = "SELECT restaurants.*, ROUND(AVG(ratings.rating)) AS average_rating, COUNT(ratings.review) AS total_reviews
     FROM ratings NATURAL JOIN restaurants
-    WHERE (name LIKE '%$searchQuery%'
-        OR tags LIKE '%$searchQuery%'
-        OR category LIKE '%$searchQuery%'
-        OR landmark LIKE '%$searchQuery%')
-    GROUP BY restaurants.restaurant_id";
+    GROUP BY restaurants.restaurant_id
+    HAVING ROUND(AVG(ratings.rating)) >= $searchQuery;";
 
     
-
-
-
-
     $result = mysqli_query($connect, $sql);
 
     $json_array = array();
